efe20d6e0d58376206dd4001149caec6e70add7a
aidara2023 <bintou1212aidara@gmail.com>
2023-05-04 13:33:30 +0000

    avant_changement

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,11 +6,11 @@ class Order < ApplicationRecord
 
   def update_total_quantity
     ActiveRecord::Base.transaction do
-    self.ordered_lists.each do |line_item|
-      item = Item.find_by(id: line_item.item_id)
-      item.total_quantity += line_item.quantity
-      item.save!
-    end
+      self.ordered_lists.each do |line_item|
+        item = Item.find_by(id: line_item.item_id)
+        item.total_quantity += line_item.quantity
+        item.save!
+      end
     end
   end
 end
